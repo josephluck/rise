21ac5f326548b51ca6a470ff564ed17638d61c11
Joseph Luck <joseph.luck@sky.com>
2017-04-14 11:17:38 +0100

    Fixup dir structure

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-require('whatwg-fetch')
-require('es6-promise')
-
-import * as sheetify from 'sheetify'
-const clx1 = sheetify('./styles/styles.css')
-const clx2 = sheetify('./styles/index.css')
-
 import helix from 'helix-react'
 import log from 'helix-react/lib/log'
 import model from './model'
@@ -12,11 +5,9 @@ import routes from './pages'
 import bootstrap from './bootstrap'
 
 let mount = document.createElement('div')
-mount.className = `${clx1} ${clx2}`
 document.body.appendChild(mount)
 
 bootstrap().then(apis => {
-  console.log('setting up helix', apis)
   helix({
     model: model(apis),
     routes,
