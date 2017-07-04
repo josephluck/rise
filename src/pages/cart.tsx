e597f2d77d8272bd9b4e66e18845b9d9fc181e63
Joseph Luck <joseph.luck@sky.com>
2017-07-04 08:59:06 +0100

    Add responsive cart / checkout view

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -28,11 +28,11 @@ const page = (mode: Mode): Helix.Page<Models> => ({
     }
   },
   view(state, prev, actions) {
-    return (
-      <div className='pb-4 ph-3'>
+    const CartItems = ({ className = '' }) => {
+      return (
         <CartItemsList
           items={state.cart.items}
-          className='mb-3 ba bc-grey-100 '
+          className={`mb-3 ba bc-grey-100 ${className}`}
           showControls={mode === 'cart'}
           updateQuantity={(item, quantity, index) => actions.cart.update({
             index,
@@ -43,114 +43,133 @@ const page = (mode: Mode): Helix.Page<Models> => ({
           })}
           removeItem={(index) => actions.cart.remove(index)}
         />
-        <Collapse
-          hasNestedCollapse
-          isOpened={mode === 'checkout'}
-        >
-          <div>
-            <div className='pb-3'>
-              <Section
-                isComplete={state.checkout.customer.valid}
-                hasErrors={!state.checkout.customer.valid && state.checkout.customer.submitted}
-                formShowing={state.checkout.sectionShowing === 1}
-                label='Your Details'
-                description={stringify(state.checkout.customer.fields)}
-                form={(
-                  <CustomerForm
-                    fields={state.checkout.customer.fields}
-                    errors={state.checkout.customer.errors}
-                    setFields={payload => {
-                      actions.checkout.customer.setFields(payload)
-                      actions.checkout.shipping.setFields(payload)
-                      actions.checkout.billing.setFields(payload)
+      )
+    }
+    return (
+      <div className='pb-4 ph-3'>
+        <CartItems className={mode === 'checkout' ? 'd-b d-n-l' : ''} />
+        <div className='d-flex'>
+          <div className='flex-2'>
+            <Collapse
+              hasNestedCollapse
+              isOpened={mode === 'checkout'}
+            >
+              <div className='pb-4-l'>
+                <div className='pb-3'>
+                  <Section
+                    isComplete={state.checkout.customer.valid}
+                    hasErrors={!state.checkout.customer.valid && state.checkout.customer.submitted}
+                    formShowing={state.checkout.sectionShowing === 1}
+                    label='Your Details'
+                    description={stringify(state.checkout.customer.fields)}
+                    form={(
+                      <CustomerForm
+                        fields={state.checkout.customer.fields}
+                        errors={state.checkout.customer.errors}
+                        setFields={payload => {
+                          actions.checkout.customer.setFields(payload)
+                          actions.checkout.shipping.setFields(payload)
+                          actions.checkout.billing.setFields(payload)
+                        }}
+                      />
+                    )}
+                    toggleFormShowing={() => {
+                      actions.checkout.setSectionShowing(1)
                     }}
                   />
-                )}
-                toggleFormShowing={() => {
-                  actions.checkout.setSectionShowing(1)
-                }}
-              />
-            </div>
-            <div className='pb-3'>
-              <Section
-                isComplete={state.checkout.shipping.valid}
-                hasErrors={!state.checkout.shipping.valid && state.checkout.shipping.submitted}
-                formShowing={state.checkout.sectionShowing === 2}
-                label='Shipping'
-                description={stringify(state.checkout.shipping.fields)}
-                form={(
-                  <ShippingForm
-                    fields={state.checkout.shipping.fields}
-                    errors={state.checkout.shipping.errors}
-                    setFields={payload => {
-                      actions.checkout.shipping.setFields(payload)
-                      actions.checkout.calculateTotals(state.cart)
-                      if (state.checkout.controls.fields.billingIsSameAsShipping) {
-                        actions.checkout.billing.setFields(payload)
-                      }
-                    }}
-                    shippingMethods={state.checkout.shippingMethods}
-                  />
-                )}
-                toggleFormShowing={() => {
-                  actions.checkout.setSectionShowing(2)
-                }}
-              />
-            </div>
-            <div>
-              <Section
-                isComplete={state.checkout.billing.valid}
-                hasErrors={!state.checkout.billing.valid && state.checkout.billing.submitted}
-                formShowing={state.checkout.sectionShowing === 3}
-                label='Billing'
-                description={stringify(state.checkout.billing.fields)}
-                form={(
-                  <BillingForm
-                    fields={state.checkout.billing.fields}
-                    errors={state.checkout.billing.errors}
-                    setFields={actions.checkout.billing.setFields}
-                    useShippingAddress={state.checkout.controls.fields.billingIsSameAsShipping}
-                    toggleUseShippingAddress={billingIsSameAsShipping => {
-                      actions.checkout.controls.setFields({
-                        billingIsSameAsShipping,
-                      })
-                      if (billingIsSameAsShipping) {
-                        actions.checkout.billing.setFields(state.checkout.shipping.fields)
-                      } else {
-                        actions.checkout.billing.setFields(fixtures.address())
-                      }
+                </div>
+                <div className='pb-3'>
+                  <Section
+                    isComplete={state.checkout.shipping.valid}
+                    hasErrors={!state.checkout.shipping.valid && state.checkout.shipping.submitted}
+                    formShowing={state.checkout.sectionShowing === 2}
+                    label='Shipping'
+                    description={stringify(state.checkout.shipping.fields)}
+                    form={(
+                      <ShippingForm
+                        fields={state.checkout.shipping.fields}
+                        errors={state.checkout.shipping.errors}
+                        setFields={payload => {
+                          actions.checkout.shipping.setFields(payload)
+                          actions.checkout.calculateTotals(state.cart)
+                          if (state.checkout.controls.fields.billingIsSameAsShipping) {
+                            actions.checkout.billing.setFields(payload)
+                          }
+                        }}
+                        shippingMethods={state.checkout.shippingMethods}
+                      />
+                    )}
+                    toggleFormShowing={() => {
+                      actions.checkout.setSectionShowing(2)
                     }}
                   />
-                )}
-                toggleFormShowing={() => {
-                  actions.checkout.setSectionShowing(3)
-                }}
+                </div>
+                <div>
+                  <Section
+                    isComplete={state.checkout.billing.valid}
+                    hasErrors={!state.checkout.billing.valid && state.checkout.billing.submitted}
+                    formShowing={state.checkout.sectionShowing === 3}
+                    label='Billing'
+                    description={stringify(state.checkout.billing.fields)}
+                    form={(
+                      <BillingForm
+                        fields={state.checkout.billing.fields}
+                        errors={state.checkout.billing.errors}
+                        setFields={actions.checkout.billing.setFields}
+                        useShippingAddress={state.checkout.controls.fields.billingIsSameAsShipping}
+                        toggleUseShippingAddress={billingIsSameAsShipping => {
+                          actions.checkout.controls.setFields({
+                            billingIsSameAsShipping,
+                          })
+                          if (billingIsSameAsShipping) {
+                            actions.checkout.billing.setFields(state.checkout.shipping.fields)
+                          } else {
+                            actions.checkout.billing.setFields(fixtures.address())
+                          }
+                        }}
+                      />
+                    )}
+                    toggleFormShowing={() => {
+                      actions.checkout.setSectionShowing(3)
+                    }}
+                  />
+                </div>
+              </div>
+              <Totals
+                className='pv-4 d-b d-n-l'
+                totals={state.checkout.totals}
               />
-            </div>
+            </Collapse>
+            {mode === 'cart'
+              ? (
+                <Button
+                  label='Checkout'
+                  className='w-100 bg-transparent ta-c bra-pill'
+                  href='/checkout'
+                />
+              ) : null
+            }
+            {mode === 'checkout'
+              ? (
+                <Button
+                  label='Pay'
+                  className='w-100 bg-transparent ta-c bra-pill'
+                  onClick={actions.checkout.submit}
+                />
+              ) : null
+            }
           </div>
-          <Totals
-            className='pv-4'
-            totals={state.checkout.totals}
-          />
-        </Collapse>
-        {mode === 'cart'
-          ? (
-            <Button
-              label='Checkout'
-              className='w-100 bg-transparent ta-c bra-pill'
-              href='/checkout'
-            />
-          ) : null
-        }
-        {mode === 'checkout'
-          ? (
-            <Button
-              label='Pay'
-              className='w-100 bg-transparent ta-c bra-pill'
-              onClick={actions.checkout.submit}
-            />
-          ) : null
-        }
+          {mode === 'checkout'
+            ? (
+              <div className='flex-1 d-n d-b-l ml-3'>
+                <CartItems />
+                <Totals
+                  totals={state.checkout.totals}
+                />
+              </div>
+            ) : null
+          }
+        </div>
       </div>
     )
   },
